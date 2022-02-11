08c814c4b512e27be8dbc8ca103e28baa504eb28
Martin Czarnecki <martin@tuffbizz.com>
2022-02-11 14:53:52 -0500

    Update userDB.py

--- userDB.py
+++ userDB.py
@@ -1,5 +1,3 @@
 users = [
-    ["Martin", "Czarnecki", "martin@tuffbizz.com"],
-    ["Matthew", "Czarnecki", "matthew@tuffbizz.com"],
-    ["Kristian", "Malec", "kristianmalec17@gmail.com"]
+    ["first-name", "last-name", "email"]
 ]
